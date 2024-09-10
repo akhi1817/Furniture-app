68d37bf01c17eaaf28d00bcccd8b777f838e7fd4
akhi1817 <akhileshkumbhar2207@gmail.com>
2024-09-11 00:01:29 +0530

    second commit

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,8 +10,6 @@ const Navbar = () => {
           <nav className="navbar navbar-expand-lg bg-success navbar-light">
             <div className="container-fluid">
               <NavLink to="/" className="navbar-brand text-white fw-bold">Furniture App</NavLink>
-              
-              {/* Toggler for mobile view */}
               <button className="navbar-toggler" type="button"data-bs-toggle="collapse"data-bs-target="#navbarNav"aria-controls="navbarNav"aria-expanded="false"aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
               <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav m-auto">
