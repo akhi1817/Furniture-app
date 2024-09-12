bd24584f5e7a37369745dceb77f04b2225d6ea47
akhi1817 <akhileshkumbhar2207@gmail.com>
2024-09-12 14:22:06 +0530

    12sept work

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-   <div className="container-fluid bg-warning">
-  <div className="row p-5">
+   <div className="container-fluid">
+  <div className="row p-5 bg-warning">
     <div className="col-md-12 d-flex flex-column flex-md-row">
       <div className="col-md-6 col-12 pt-5">
         <h1 className="text-white text-center text-md-start">Modern Interior Design<br />Studio</h1>
@@ -23,9 +23,78 @@ const Home = () => {
       </div>
     </div>
   </div>
-</div>
-
-    
+  <div className='col-md-12 p-5'>
+    <div className='row mt-5'>
+      <div className='col-md-12 d-flex'>
+        <div className='col-md-3 p-3'>
+          <h2>Crafted with excellent material.</h2>
+          <p>Welcome to our exquisite range of furniture, where style meets functionality. Our collection features a diverse array of high-quality pieces designed to enhance any living space.
+             Whether you're looking for modern, classic, or contemporary designs, our selection has something to suit every taste and need.</p>
+          <button className='btn btn-dark  rounded-pill px-5 py-2 mx-3'>Explore</button>
+        </div>
+        <div className='col-md-3 p-3'>
+          <div className='card'>
+            <div className='card-body text-center'>
+              <img src='images/product-1.png' className='img-fluid' style={{width:'300px',height:'300px'}}/>
+              <h5>Nordic Chair</h5>
+              <h2>$50.00</h2>
+            </div>
+            <div className='card-footer text-center'>
+              <button className='btn btn-success rounded px-2 mx-2'>Add to Cart</button>
+              <button className='btn btn-primary rounded px-2 mx-2'>Buy Now</button>
+              
+            </div>
+          </div>
+        </div>
+        <div className='col-md-3 p-3'>
+          <div className='card'>
+            <div className='card-body text-center'>
+              <img src='images/product-2.png' className='img-fluid' style={{width:'300px',height:'300px'}}/>
+              <h5>Kruzo Aero Chair</h5>
+              <h2>$78.00</h2>
+            </div>
+            <div className='card-footer text-center'>
+              <button className='btn btn-success rounded px-2 mx-2'>Add to Cart</button>
+              <button className='btn btn-primary rounded px-2 mx-2'>Buy Now</button>
+              
+            </div>
+          </div>
+        </div>
+        <div className='col-md-3 p-3'>
+          <div className='card'>
+            <div className='card-body text-center'>
+              <img src='images/product-3.png' className='img-fluid' style={{width:'300px',height:'300px'}}/>
+              <h5>Ergonomic Chair</h5>
+              <h2>$43.00</h2>
+            </div>
+            <div className='card-footer text-center'>
+              <button className='btn btn-success rounded px-2 mx-2'>Add to Cart</button>
+              <button className='btn btn-primary rounded px-2 mx-2'>Buy Now</button>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className='col-md-12 p-5'>
+    <div className='row'>
+      <div className='col-md-12'>
+        <div className='col-md-6'>
+            <h1>Why Choose Us</h1>
+            <p className='text-secondary'>Choose us for top-quality furniture that combines durability with style at affordable prices. 
+              Our diverse selection caters to every taste, from modern to classic, and our customization options ensure your 
+              pieces reflect your unique taste. Enjoy a seamless shopping experience with excellent customer service and 
+              reliable delivery,making us your go-to choice for exceptional furniture and outstanding service.</p>
+            <div className='col-md-3'>
+            <i class="bi bi-truck fs-1"></i>
+            
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>    
     </>
   )
 }
